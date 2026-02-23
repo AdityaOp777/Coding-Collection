259a45f264891d21062d82ddefb1f7089976202c
AdityaOp777 <adityagavhane91@gmail.com>
2026-02-23 23:40:51 +0530

    Added JS Folder

---
+++ JavaScript/1.js
@@ -0,0 +1,19 @@
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers);
+
+console.log(numbers[0]); // Accessing the first element
+
+console.log(numbers[numbers.length - 1]); // Accessing the last element
+
+numbers.push(6); // Adding a new element to the end of the array
+
+console.log(numbers);
+
+numbers.pop(); // Removing the last element from the array
+
+console.log(numbers);
+
+numbers.unshift(0); // Adding a new element to the beginning of the array
+
+console.log(numbers);
